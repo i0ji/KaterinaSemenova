cb977a138ce37becf890b0a099dcadf57484f6bd
ioji <ioji@inbox.ru>
2023-06-25 15:29:04 +0300

    small touches; some query attempts; refactoring adaptive

--- src/components/Section/Seagull/Seagull.tsx
+++ src/components/Section/Seagull/Seagull.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import Carousel from '../../Carousel/Carousel';
 import styles from './Seagull.module.scss';
-import {seagullSlides, seagullDescription} from './SeagullData';
+import {slides, SeagullDescription} from './SeagullData'
+
 
 function Seagull() {
 
-    return (
-        <div className={`${styles.section} ${styles.section_margin}`}>
-            <Carousel description={seagullDescription}>
-                {
-                    seagullSlides.map((item, index) => {
-                        return (
-                            <div>
-                                <img src={item.img} alt={'Чайка. Фирменный стиль, 2022 г.'}/>
-                            </div>
-                        )
-                    })
 
-                }
-            </Carousel>
+    return (
+            <div className={`${styles.section} ${styles.section_margin}`}>
+                <Carousel description={SeagullDescription}>
+                    {
+                        slides.map((item, index) => {
+                            return (
+                                <div>
+                                    <img src={item.img} alt={'Чайка. Фирменный стиль, 2022 г.'}/>
+                                </div>
+                            )
+                        })
+                    }
+                </Carousel>
         </div>
     )
 }
