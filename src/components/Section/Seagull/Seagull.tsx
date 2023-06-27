e4cc9845d6eab49ca9cc220f5ea62084dd637c56
ioji <ioji@inbox.ru>
2023-06-27 15:06:12 +0300

    new carousel module added; responsive-carousel npm isntalled; new carousel attempt

--- src/components/Section/Seagull/Seagull.tsx
+++ src/components/Section/Seagull/Seagull.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import Carousel from '../../Carousel/Carousel';
+import React, { useState } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {Carousel} from "react-responsive-carousel";
 import styles from './Seagull.module.scss';
 import {slides, SeagullDescription} from './SeagullData'
 
+const renderSlides = slides.map((image) => (
+    <div>
+        <img src={image.img} alt={SeagullDescription} />
+    </div>
+));
 
-function Seagull() {
+export default function App() {
+    const [currentIndex, setCurrentIndex] = useState();
 
-
+    function handleChange(index: any) {
+        setCurrentIndex(index);
+    }
     return (
-            <div className={`${styles.section} ${styles.section_margin}`}>
-                <Carousel description={SeagullDescription}>
-                    {
-                        slides.map((item, index) => {
-                            return (
-                                <div>
-                                    <img src={item.img} alt={'Чайка. Фирменный стиль, 2022 г.'}/>
-                                </div>
-                            )
-                        })
-                    }
-                </Carousel>
+        <div className={`${styles.section} ${styles.section_margin}`}>
+            <Carousel
+                showArrows={true}
+                autoPlay={true}
+                infiniteLoop={true}
+                selectedItem={slides[currentIndex]}
+                onChange={handleChange}
+                className="carousel-container"
+            >
+                {renderSlides}
+            </Carousel>
         </div>
-    )
+    );
 }
-
-export default Seagull;
