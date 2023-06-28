3aa857e88f32874da2f74784ad928b9e2fcbc1c7
ioji <ioji@inbox.ru>
2023-06-28 23:55:39 +0300

    all sections are brought to the Seagull point;

--- src/components/Section/Mall/Mall.tsx
+++ src/components/Section/Mall/Mall.tsx
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import {Carousel} from "react-responsive-carousel";
 import styles from './Mall.module.scss';
 import {slides, mallDescription} from './MallData';
 import '../../../styles/_carouselStyles.scss'
+import SlideCarousel from "../../Carousel/Carousel";
 
 
 const renderSlides = slides.map((image) => (
@@ -13,39 +13,15 @@ const renderSlides = slides.map((image) => (
 ));
 
 function Mall() {
-    const [currentIndex, setCurrentIndex] = useState();
-
-    function handleChange(index: any) {
-        setCurrentIndex(index);
-    }
-
-
     return (
         <div className={`${styles.section} ${styles.section_margin}`}>
 
-
-            <Carousel
-                showArrows={true}
-                autoPlay={true}
-                interval={5000}
-                showThumbs={false}
-                showStatus={false}
-                infiniteLoop={true}
-                onChange={handleChange}
-                showIndicators={false}
-                className={styles.CarouselStyles}
-            >
-
-                {renderSlides}
-
-            </Carousel>
-
+            <SlideCarousel render={renderSlides}/>
 
             <div className={styles.section_description}>
                 <p>{mallDescription}</p>
 
-                <p>{`${Number(currentIndex)+1} 
-                / ${slides.length}`}</p>
+                <p>{`${slides.length}`}</p>
             </div>
         </div>
     )
