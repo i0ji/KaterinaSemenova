61ab799ba9f781c46052fd7ba7d284079854db29
ioji <ioji@inbox.ru>
2023-08-20 15:49:37 +0300

    deprecating theme slider

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import styles from "./Header.module.scss"
 import {Link} from "react-scroll";
-import ThemeToggle from "../ThemeToggle/ThemeToggle";
 
 export default function Header() {
+
+
 
     return (
         <header className={styles.header}>
             <Link to={'about'}>
                 Катерина Семёнова
             </Link>
-
-            <div className={styles.header__toggle_bar}>
-                <ThemeToggle/>
-            </div>
         </header>
     )
 }
