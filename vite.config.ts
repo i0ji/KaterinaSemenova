dcf6ac065722490d7a8d0b6b4013c08ada1fca47
i0ji <ioji@inbox.ru>
2023-10-10 16:26:46 +0300

    font set

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
+const path = require("path");
+
 export default defineConfig({
-    base: 'src',
+    base: '',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        }
+    },
     plugins: [
         viteTsconfigPaths(),
     ],
